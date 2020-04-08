f7606758ec50c6c91982594c9dcef8b4264d4623
JonJakova <50449118+JonJakova@users.noreply.github.com>
2020-04-08 23:05:57 +0200

    fix

--- controllers/register.js
+++ controllers/register.js
@@ -6,6 +6,8 @@ const handleRegister = (req, res, db, bcrypt) => {
     return res.status(400).json('incorrect form submission');
   }
   const hash = bcrypt.hashSync(password);
+  console.log('hash', hash);
+  
 
     db.transaction(trx => {
       trx.insert({
