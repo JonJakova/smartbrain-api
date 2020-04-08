60d0266c596521225d7c3b8db97376c5c6ff05e6
JonJakova <50449118+JonJakova@users.noreply.github.com>
2020-04-08 21:42:18 +0200

    fix

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: process.env.DB_PASSWORD,
-        database: 'smart-brain'
+       connectionString: process.env.DATABASE_URL,
+       ssl: true
     }
 });
 
