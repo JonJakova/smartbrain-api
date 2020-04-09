343a00a3c3a71722fd78e5c1d9e0dca92a2d3a77
JonJakova <50449118+JonJakova@users.noreply.github.com>
2020-04-09 16:29:14 +0200

    deploy

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const knex = require('knex');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT;
+const whitelist = ['https://rocky-hamlet-85621.herokuapp.com/'];
+const corsOption = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -23,10 +33,9 @@ const db = knex({
 app.use(express.json());
 app.use(cors());
 
-// app.get('/', (req, res) => {res.send(db.select('*').from('login').then(data => console.log(data)))})
 app.get('/', (req, res) => {res.send('it is working')})
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.post('/signin', cors(corsOption), signin.handleSignin(db, bcrypt))
+app.post('/register', cors(corsOption), (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
