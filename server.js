cedc34a0213920f46d8aeaef49cad4c219ac464d
JonJakova <50449118+JonJakova@users.noreply.github.com>
2020-04-09 13:51:30 +0200

    debugging

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
+        host: process.env.DATABASE_URL,
         ssl: true,
     }
 });
